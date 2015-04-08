a84ea3e36b8333262c2de4fd14d31a0e080373d3
ngpanwei <dev@ngpanwei.com>
2015-04-08 20:14:53 +0800

    activity page

--- public_html/page/home.php
+++ public_html/page/home.php
@@ -1,11 +1,18 @@
 <div data-role="page" id="pgPersonHome" data-title="个人设置">
-  <div data-role="header" data-position="fixed" data-theme="a">
-    <h1>我的活动<br/>xxx</h1>
-    <a id="syncBtn" data-role="button" data-icon="refresh" class="ui-btn-right">更新</a>
-  </div>
-  <div data-role="main" class="ui-content">
-    <div id="activities">
-    </div>
-  </div>
-<!-- footer -->
+	<div data-role="header" data-position="fixed" data-theme="a">
+		<h1>我的活动</h1>
+		<a href="#activityPopupMenu" data-rel="popup" data-role="button" data-icon="info"
+			class="ui-btn-right">处理</a>
+	</div>
+	<div data-role="popup" id="activityPopupMenu" data-theme="a">
+		<ul data-role="listview" data-inset="true" style="min-width: 100px;">
+			<li><a id="syncBtn">刷新</a></li>
+			<li><a id="shareBtn">分享</a></li>
+		</ul>
+	</div>
+	<div data-role="main" class="ui-content">
+		<div id="others"></div>
+		<div id="activities"></div>
+	</div>
+	<!-- footer -->
 </div>
