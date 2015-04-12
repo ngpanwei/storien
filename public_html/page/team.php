ddc7a5f46f3d68c51f6946f257b12abdc7b487f6
ngpanwei <dev@ngpanwei.com>
2015-04-12 17:57:22 +0800

    presentation enhancement

---
+++ public_html/page/team.php
@@ -0,0 +1,16 @@
+<div data-role="page" class="ui-page-theme-a" id="pgTeamHome" data-title="团队">
+	<div data-role="header" data-position="fixed" data-theme="a">
+		<h1>我的团队</h1>
+		<a href="#teamPopupMenu" data-rel="popup" data-role="button" data-icon="info"
+			class="ui-btn-right">处理</a>
+	</div>
+	<div data-role="popup" id="teamPopupMenu" data-theme="a">
+		<ul data-role="listview" data-inset="true" style="min-width: 100px;">
+			<li><a id="syncTeamBtn">刷新</a></li>
+			<li><a id="adminBtn">管理</a></li>
+		</ul>
+	</div>
+	<div data-role="main" class="ui-content">
+	</div>
+	<!-- footer -->
+</div>
